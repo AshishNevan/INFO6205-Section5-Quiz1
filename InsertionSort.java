085198131e07a375cf9152ae2c661429ff8d16f7
harshithptl <42831234+harshithptl@users.noreply.github.com>
2025-01-22 16:39:26 -0500

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class InsertionSort {
+    //Sorts the array using Insertion Sort.
+    public void sort(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            insert(i, a);
+        }
+    }
+
+    private void insert(int i, Comparable[] a) {
+        // Inserts the 'transition element' into its correct position in the sorted portion of the array.
+        Comparable temp = a[i];
+        int j = i - 1;
+
+        // Shift elements of the sorted portion to the right until the correct position is found
+        while (j >= 0 && a[j].compareTo(temp) > 0) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        // Place the transition element at its correct position
+        a[j + 1] = temp;
+    }
+    
+    private void swap(Object[] a, int i, int j) {
+        Object temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
